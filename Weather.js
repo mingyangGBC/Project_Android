cc24f9e5503933a2262939068d16274990edfefd
mingyangGBC <79671601+mingyangGBC@users.noreply.github.com>
2021-12-08 19:29:55 -0500

    First Update
    fetch 7 days of information as temperature, weather and icon.
    Get current location,
    get current date,
    get current day,
    get location's city and country

---
+++ Weather.js
@@ -0,0 +1,112 @@
+import React, { Component, useState } from 'react'
+import { SafeAreaView, Text, Image } from 'react-native'
+import * as Location from 'expo-location';
+
+export default class Weather extends Component {
+
+    constructor() {
+        super()
+        this.state={
+            city:undefined,
+            country: undefined,
+            // 7 days of the temperature
+            forecast_temp:[],
+            // icon id for seven days
+            forecast_icon:[],
+            // weather for seven days
+            forecast_main:[],
+            lon:0,
+            lat:0,
+        }
+        this.getWeather()
+    }
+
+    
+    getWeather = async() =>{
+
+        
+        //https://pro.openweathermap.org/data/2.5/forecast/climate?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${API_KEY} (for lat, lon )
+        //https://pro.openweathermap.org/data/2.5/forecast/climate?q=Toronto&appid=${API_KEY} (for specific city test)
+        const location = await Location.getCurrentPositionAsync()
+        const API_KEY = '5a9f3d77f65a849f70e24fd83bd9d3b0'
+        const api = await fetch(`https://pro.openweathermap.org/data/2.5/forecast/climate?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${API_KEY}`)
+        const response = await api.json()
+        console.log(response)
+
+        var today = new Date(),
+        date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+        this.setState({
+            city: response.city.name,
+            country: response.city.country,
+            currentDate: date,
+            currentDay: this.getDay(today.getDay()),
+            lon: location.coords.longitude,
+            lat: location.coords.latitude
+        })
+        for(let i = 0; i < 7; i++){
+            const tempory_temp = [this.calculateCel(response.list[i].temp.eve)]
+            const tempory_main = [response.list[i].weather[0].main]
+            const tempory_icon = [response.list[i].weather[0].icon]
+            this.setState({
+                forecast_temp:[...this.state.forecast_temp, tempory_temp],
+                forecast_main:[...this.state.forecast_main, tempory_main],
+                forecast_icon:[...this.state.forecast_icon, tempory_icon],
+            })
+        }
+      }
+
+    getDay(day){
+        let day1 = ''
+        switch(true){
+            case day === 0:
+                day1 = 'Sunday'
+                break
+            case day === 1:
+                day1 = 'Monday'
+                break
+            case day === 2:
+                day1 = 'Tuesday'
+                break
+            case day === 3:
+                day1 = 'Wednesday'
+                break
+            case day === 4:
+                day1 = 'Thursday'
+                break
+            case day === 5:
+                day1 = 'Friday'
+                break
+            case day === 6:
+                day1 = 'Saturday'
+                break
+        }
+        return day1
+    }
+
+    calculateCel(temp){
+        let cel = Math.floor(temp-273.15)
+        return cel
+    }
+
+    getIcon(icon){
+        let url = `http://openweathermap.org/img/wn/${icon}@2x.png`
+        return url
+    }
+
+    render() {
+        return (
+            <SafeAreaView>
+                <Text>Weather APP</Text>
+                <Text>{this.state.city}, {this.state.country}</Text>
+                <Text>{this.state.forecast_temp[0]} Degree</Text>
+                <View><Image source={require(`@${this.getIcon(forecast_icon[0])}`)}/></View>
+                <Text>Today's weather: {this.state.forecast_main[0]}</Text>
+                <Text>{this.state.currentDate}</Text>
+                <Text>{this.state.currentDay}</Text>
+                <Text>{this.state.lat}{this.state.lon}</Text>
+            </SafeAreaView>
+        )
+    }
+}
+
